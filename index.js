8d19b5b8b850d49b98c2ff90b257a7510302fff8
Paul Hsueh <hsueh.paul@gmail.com>
2021-05-17 14:23:30 -0400

    commit tree 97404847358a29afeb4f45d683a20d2f24ce6a2a

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return new sanitizer.sanitizeToString(string); 
+  }
+
+  static titleize(String) {
+
+  }
+
 }
